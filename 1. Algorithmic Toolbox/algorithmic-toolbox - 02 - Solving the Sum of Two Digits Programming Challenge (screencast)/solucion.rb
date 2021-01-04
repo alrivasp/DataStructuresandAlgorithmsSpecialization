058a3663a597099234f2f6eb919a0c8b7311eb4f
alrivasp <alvaro.rivas.p@gmail.com>
2021-01-03 22:07:10 -0300

    First pŕoblem resolve of course

---
+++ 1. Algorithmic Toolbox/algorithmic-toolbox - 02 - Solving the Sum of Two Digits Programming Challenge (screencast)/solucion.rb
@@ -0,0 +1,7 @@
+#El problema se denomina A más B. En este problema, te damos dos dígitos en la entrada estándar y nuestro objetivo es mostrar su suma en la salida estándar.
+
+def sum(a, b)
+return a+b
+end
+
+puts sum(10,5)
